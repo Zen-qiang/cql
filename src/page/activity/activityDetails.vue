673a38e814080902f81a142cc47763c4a5894ea9
dinglian <2425156099@qq.com>
2017-09-01 23:17:14 +0800

    详情分享

--- src/page/activity/activityDetails.vue
+++ src/page/activity/activityDetails.vue
@@ -94,7 +94,7 @@
   import 'moment/locale/zh-cn'
   moment.locale('zh-cn')
   import * as types from '../../store/mutation-types'
-//  import wx from 'weixin-js-sdk'
+  import wx from 'weixin-js-sdk'
   export default {
     filters: {
       moment (val) {
@@ -134,6 +134,36 @@
       this.getActivityInfo()
     },
     mounted () {
+      wx.ready(function () {
+        wx.showAllNonBaseMenuItem()
+//        朋友圈
+        wx.onMenuShareTimeline({
+          title: this.activityInfo.name, // 分享标题
+          link: window.location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+          imgUrl: '', // 分享图标
+          success: function () {
+            // 用户确认分享后执行的回调函数
+          },
+          cancel: function () {
+            // 用户取消分享后执行的回调函数
+          }
+        })
+//        朋友
+        wx.onMenuShareAppMessage({
+          title: this.activityInfo.name, // 分享标题
+          desc: this.activityInfo.description, // 分享描述
+          link: window.location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+          imgUrl: '', // 分享图标
+          type: '', // 分享类型,music、video或link，不填默认为link
+          dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+          success: function () {
+            // 用户确认分享后执行的回调函数
+          },
+          cancel: function () {
+            // 用户取消分享后执行的回调函数
+          }
+        })
+      })
     },
     methods: {
       preventSwitch () {
--- src/page/circle/circleDetails.vue
+++ src/page/circle/circleDetails.vue
@@ -36,6 +36,7 @@
   import CircleEvents from '../../components/baseCircle/circleEvents.vue'
   import * as types from '../../store/mutation-types'
   import { Toast } from 'mint-ui'
+  import wx from 'weixin-js-sdk'
   export default {
     components: {
       CircleEvents
@@ -65,6 +66,38 @@
       selected: function (val, oldVal) {
         this.getTopicList(val)
       }
+    },
+    mounted () {
+      wx.ready(function () {
+        wx.showAllNonBaseMenuItem()
+//        朋友圈
+        wx.onMenuShareTimeline({
+          title: this.circle.name, // 分享标题
+          link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+          imgUrl: '', // 分享图标
+          success: function () {
+            // 用户确认分享后执行的回调函数
+          },
+          cancel: function () {
+            // 用户取消分享后执行的回调函数
+          }
+        })
+//        朋友
+        wx.onMenuShareAppMessage({
+          title: this.circle.name, // 分享标题
+          desc: this.circle.description, // 分享描述
+          link: window.location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+          imgUrl: '', // 分享图标
+          type: '', // 分享类型,music、video或link，不填默认为link
+          dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+          success: function () {
+            // 用户确认分享后执行的回调函数
+          },
+          cancel: function () {
+            // 用户取消分享后执行的回调函数
+          }
+        })
+      })
     },
     created () {
       this.loadCircleInfo(this.circleId)
