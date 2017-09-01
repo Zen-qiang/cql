53d3a97447e8d5e14578f63bd8dde229e7ba8612
dinglian <2425156099@qq.com>
2017-09-01 18:51:15 +0800

    11

--- src/page/activity/editContent.vue
+++ src/page/activity/editContent.vue
@@ -127,17 +127,19 @@
     },
     methods: {
       paizhao () {
-        Toast('dianjil')
+        Toast('paizhao')
         wx.chooseImage({
           count: 1, // 默认9
           sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
           sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
           success: function (res) {
             this.localIds = res.localIds
-            Toast(this.localIds)
-            Toast(res.localIds)
+            Toast(this.localIds + '1')
+            Toast(res.localIds + '2')
+            Toast(res + '3')
           }
         })
+        Toast(this.localIds + '4')
       },
 //        判断移动设备
       judgmentIos () {
