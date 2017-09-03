50de07933e38a5d2539274434311b2342ae9f695
dinglian <2425156099@qq.com>
2017-09-03 10:22:13 +0800

    1

--- src/page/activity/editContent.vue
+++ src/page/activity/editContent.vue
@@ -165,7 +165,7 @@
         if (this.ready) {
           alert('ready')
           wx.chooseImage({
-            count: 3, // 默认9
+            count: 1, // 默认9
             sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
             sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
             success: function (res) {
