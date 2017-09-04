cd0c7a35df2530fb25a98966091f6aefc5732078
frank <2425156099@qq.com>
2017-09-04 21:53:11 +0800

    去除vconsole

--- src/main.js
+++ src/main.js
@@ -15,8 +15,6 @@ import axios from './http'
 import * as types from './store/mutation-types'
 import cookie from './utils/cookie'
 
-import 'vconsole'
-
 // Vue.use(axios)
 // 将axios挂载到prototype上，在组件中可以直接使用this.axios访问
 Vue.prototype.axios = axios
