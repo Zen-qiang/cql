9c30a7a202380ab1b1593a83e50dffb5a97d7707
frank <2425156099@qq.com>
2017-09-07 14:01:15 +0800

    ddd

--- src/page/activity/signUpActivity.vue
+++ src/page/activity/signUpActivity.vue
@@ -93,8 +93,7 @@
     created () {
       this.activity = this.$store.state.activity
       this.userName = this.$store.state.userName
-      alert(this.$store.state.userPhoneNo)
-      if (this.$store.state.userPhoneNo) {
+      if (this.$store.state.userPhoneNo && this.$store.state.userPhoneNo !== 'null') {
         this.telphone = this.$store.state.userPhoneNo
       } else {
         this.needBind = true
